951fc733d8f8c1b76cfb9064f8a8c5de36cd8048
Tegegn <12001002972705@FEDIDCARD.GOV>
2022-01-04 15:29:36 -0500

    update settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -276,7 +276,9 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+#$drupal_hash_salt = '';
+
+$settings['hash_salt'] = 'GoHCAodnCITJVdd1C-EBlQeRq9RmcB0ihWASnBMLoSDgH4xvAU5TqTTyV0rCTmENjTOGQcWUGqQ';
 
 /**
  * Base URL (optional).
